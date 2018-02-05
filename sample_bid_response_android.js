14201be40f70fec9aebb14e9dc9ebdec73240c9e
brittanyzellman <bzellman@triplelift.com>
2018-02-05 16:47:10 -0500

    sample bid response for questionnaire

---
+++ sample_bid_response_android.js
@@ -0,0 +1,27 @@
+HTTP/1.1 200 OK
+content-type: application/json; charset=utf-8
+content-length: 2274
+content-encoding: gzip
+date: Mon, 5 Feb 2018 21:39:24 GMT
+
+{
+  "id": "d0d08e77b0f94b39afc61f3c4817535b",
+  "seatbid": [
+    {
+      "bid": [
+        {
+          "id": "6157623911467319901",
+          "impid": "42e40a7e-6a57-4790-8cd8-14ee6454f233",
+          "price": 0.121,
+          "adm": "<script>document.createElement('IMG').src=\"https://tlx.3lift.com/s2s/notify?px=1&aid=6157623911467319901&n=GvwFaHR0cDovL2ltcC5iaWQuYWNlLmFkdmVydGlzaW5nLmNvbTo4MC9tcHcvc2l0ZT05NjExNDcvcmVxdWVzdElkPTYxNTc2MjM5MTE0NjczMTk5MDEtMzcwOS05NzAyNC9iaWRJZD0vaW1wSWQ9MS9waWQ9MTAxOS9zZWF0SWQ9MS9hZElkPS90bGNwbXByaWNlPSR7QVVDVElPTl9QUklDRX0vc2l6ZT0xOTExMDAvdGFncz00OS9yaXA9Q2hSalpERTJNREF4TlRFek9UTTBNVGN4TURBd05oSWxRVk0wTWpFMk1qazJOVFptWmpJeVpEUTBPVFkyT1dZeVpqUTJNalJpWkRZNU9EQXdaaGo3QnlJQk1Tb0FNZzFCY0hBZ1VHeGhZMlZ0Wlc1ME9ObjRyUUZBM0FsSUFIMDlMVlU1blFFOUxWVTVwUUZrbEJzX3JRRUFBQUFBM1FFQUFBQUE2QUVCX1FFOUxWVTVtZ0lrUVRjeFJqQTBRakl0TWtNNFJDMDBOa0V5TFRoRU9ERXRPVVZDTVVFelJEWTBNell5eWdJSElDeG1ZV3h6WmRJQ0J5QXNabUZzYzJYaUFoOWxiVzlxYVM1clpYbGliMkZ5WkM1bGJXOTBhV052Ym10bGVXSnZZWEprNmdJSGNISnBiV0Z5ZWZJQ0FQb0NJa0ZUTkRJeE5qSTVPR0l5TjJSbE5Ea3hNR0U1T0RReE5USmxNekpoTkRabU5UQ2FBd2t6TWk0M016VTROamVpQXdrdE9UY3VORGM0T1RXcUF3TmhjSEN5QXg5bGJXOXFhUzVyWlhsaWIyRnlaQzVsYlc5MGFXTnZibXRsZVdKdllYSmt5Z01XYzJWc1pXTjBMV1JsWVd4UWNtbHZjbWwwYVhwbFpBL3JjPTFGQ0UyMDQ3NTc4MDM5MURGOUJFQTRFMDI1M0I0OTZCL2J6PV92OEFkUUJ6QUMwQVpRQmhBSE1BZEFBdEFERUFaQS9saW1pdGFkdHJhY2tpbmc9ZmFsc2XyAocBCAASEzYxNTc2MjM5MTE0NjczMTk5MDEYACABKP0cMP11QAFIAFAAYAFoAHAGeJFCkAGM%2FCaYAYWtEKAB6q8YqAHhre0BsAHLAbgBGcABecgBywHgASPwAQD4AcsBgAJ5iAIjkQIAAAAAAADwP5kCAAAAoJmZ2T%2BhAvI92CXX%2Fu8%2FqAIAsAIA%2BAIG\";window.tl_auction_response_587683=\"dynamicCreativeRender({\\\"settings\\\":{\\\"advertiser_name\\\":\\\"Allstate\\\",\\\"viewability\\\":{},\\\"impression_pixels\\\":[\\\"https:\\/\\/ad.doubleclick.net\\/ddm\\/trackimp\\/N5398.3020245OATH.COM\\/B20559517.211008795;dc_trk_aid=410478837;dc_trk_cid=96113676;ord=8821413735;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=?\\\",\\\"https:\\/\\/pixel.adsafeprotected.com\\/rfw\\/st\\/131733\\/20626854\\/skeleton.gif\\\"],\\\"type\\\":\\\"image\\\",\\\"additional_data\\\":{\\\"sid\\\":\\\"15101\\\",\\\"bmid\\\":\\\"3709\\\",\\\"ts\\\":\\\"1517866764\\\",\\\"brid\\\":\\\"6\\\",\\\"tid\\\":\\\"638476\\\",\\\"aid\\\":\\\"6157623911467319901\\\"},\\\"logo_image_url\\\":\\\"\\/\\/images.3lift.com\\/3592717.png\\\",\\\"logo_width\\\":300,\\\"logo_height\\\":300,\\\"format_id\\\":1,\\\"render_options_bm\\\":0},\\\"assets\\\":[{\\\"image_id\\\":3888865,\\\"image_url\\\":\\\"\\/\\/images.3lift.com\\/3888865.jpg\\\",\\\"image_width\\\":1024,\\\"image_height\\\":535,\\\"heading\\\":\\\"Auto emergency checklist\\\",\\\"caption\\\":\\\"No one enjoys being stranded on the road, but when you have a well-equipped emergency car kit, the situation can be more bearable.\\\",\\\"clickthrough_url\\\":\\\"http:\\/\\/imp.bid.ace.advertising.com:80\\/click\\/site=961147\\/mnum=2849881\\/bnum=1238981298\\/cstr=1238981298=_5a78cf0c_1238981298_961147*2849881*1244*0_1_\\/deviceid=A71F04B2-2C8D-46A2-8D81-9EB1A3D64362\\/bz=_v8AdQBzAC0AZQBhAHMAdAAtADEAZA\\/limitadtracking=false\\/app_bundle=emoji.keyboard.emoticonkeyboard\\/imptid=AS4216298b27de4910a984152e32a46f50\\/optn=64?trg=https%253A%252F%252Fblog.allstate.com%252Fsuper-vehicle-emergency-car-kit%252F%253FCID%253DBAC-CON-CROSS-OTH-AU-180101\\\",\\\"img_server_params\\\":\\\"v=17&logo=%2F%2Fimages.3lift.com%2F3592717.png&noCrop=1\\\"}]});\";</script><script src=\"//ib.3lift.com/ttj?inv_code=TLXAPP_emoji_keyboard_emoticonkeyboard_300x250\" data-auction-response-id=\"587683\"></script>",
+          "adomain": [
+            "https://blog.allstate.com"
+          ],
+          "iurl": "//images.3lift.com/3888865.jpg",
+          "crid": "3709_15101_T638476"
+        }
+      ]
+    }
+  ],
+  "cur": "USD"
+}
